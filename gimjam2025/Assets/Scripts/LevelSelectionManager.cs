1dfb9608373bc53cdf5d861285535e6def109063
stapzuwi <ashkarstation@gmail.com>
2025-02-21 15:29:32 +0700

    making level button uninteractable when it hasn't been cleared

--- gimjam2025/Assets/Scripts/LevelSelectionManager.cs
+++ gimjam2025/Assets/Scripts/LevelSelectionManager.cs
@@ -1,11 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelectionManager : MonoBehaviour
 {
+    public static LevelSelectionManager instance {get; private set;}
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public Button[] levelButtons;
+    public static int highestLevelCleared;
+    [SerializeField] private int levelSceneIndex;
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject levelSelection;
+
+    void Start()
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i > highestLevelCleared)
+            {
+                levelButtons[i].interactable = false;
+            }
+        }
+    }
 
     public void SelectLevel()
     {
