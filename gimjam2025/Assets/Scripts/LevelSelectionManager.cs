5bd59ff6e7794c375af03cfd9702c1e81d2efe48
stapzuwi <ashkarstation@gmail.com>
2025-02-23 10:59:16 +0700

    fixing level select

--- gimjam2025/Assets/Scripts/LevelSelectionManager.cs
+++ gimjam2025/Assets/Scripts/LevelSelectionManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LevelSelectionManager : MonoBehaviour
 {
@@ -19,13 +20,30 @@ public class LevelSelectionManager : MonoBehaviour
         }
     }
 
-    public Button[] levelButtons;
+    public GameObject levelButton;
     public List<Level> levels;
-    public int highestLevelCleared;
+    [SerializeField] private List<Image> image;
+    [SerializeField] private List<GameObject> image3D;
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject levelSelection;
+    [SerializeField] private Transform content;
+    [SerializeField] private Transform UI3D;
+    [SerializeField] private LoadingScreen loadingScreen;
 
     void Start()
+    {
+        levels = new List<Level>
+        {
+            new Level("Level 1", image[0], true, "Level 1", image3D[0]),
+            new Level("Level 2", image[1], true, "Level 2", image3D[1]),
+            new Level("Level 3", image[2], false, "Level 3", image3D[2]),
+            new Level("Level 4", image[3], false, "Level 4", image3D[3]),
+            new Level("Level 5", image[4], false, "Level 5", image3D[4]),
+            new Level("Level 6", image[5], false, "Level 6", image3D[5])
+        };
+        UpdateLevels();
+    }
+    /*void Start()
     {
         for (int i = 0; i < levelButtons.Length; i++)
         {
@@ -34,16 +52,35 @@ public class LevelSelectionManager : MonoBehaviour
                 levelButtons[i].interactable = false;
             }
         }
-    }
+    }*/
 
     public void SelectLevel()
     {
         mainMenu.SetActive(false);
         levelSelection.SetActive(true);
+        UpdateLevels();
     }
 
     public void UpdateLevels()
     {
-
+    foreach(Transform child in content)
+    {
+        Destroy(child.gameObject);
+    }
+    foreach(Level level in levels)
+    {
+        GameObject newObject = Instantiate(levelButton);
+        newObject.transform.SetParent(content);
+        TMP_Text text = newObject.transform.GetChild(1).GetComponent<TMP_Text>();
+        text.text = level.displayText;
+        Button button = newObject.GetComponent<Button>();
+        button.onClick.AddListener(()=>{
+            loadingScreen.LoadLevelBtn(level.scene);
+        });
+        if(!level.isUnlocked)
+        {
+            button.interactable = false;
+        }
+    }
     }
 }
